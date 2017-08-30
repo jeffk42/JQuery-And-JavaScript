158fc29b6e6570fadd6d7a10f18d9a19e6bf19f1
Jeff Kraus <jeffk42@gmail.com>
2017-08-29 20:23:24 -0400

    hour0801 additions

---
+++ hour08/hour0801.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	// get first button, set click function to iterate through p tags and set CSS attributes.
+	$("input:eq(0)").click(function () {
+		$("p").each(function() {
+			var parts = $(this).html().split(" ");
+			$(this).css({"font-size":parts[1]+"px", "color":parts[0]});
+		})
+	})
+	
+	// get second button
+	$("input:eq(1)").click(function () {
+		var items = $("p").map(function () {
+			var parts = $(this).html().split(" ");
+			return {color:parts[0], size:parts[1]};
+		}).get();
+		
+		for (var idx in items) {
+			var item = items[idx];
+			var span = $("<span>" + item.color + "</span>");
+			var size = item.size * 5;
+			span.css({"background-color":item.color, "font-size":item.size+"px", width:size, height:size});
+			$("div").append(span);
+		}
+	})
+})
