e775db775fdf057875fdaf7e41e01f358e889803
Jeff Kraus <jeffk42@gmail.com>
2017-08-31 21:35:53 -0400

    add 09-04

---
+++ hour09/hour0904.js
@@ -0,0 +1,13 @@
+function inputHandler(e) {
+	var chr = String.fromCharCode(e.charCode);
+	$("p").append(chr);
+}
+
+function spanHandler(e) {
+	var chrCode = e.target.innerHTML.charCodeAt(0);
+	$("input").trigger({'type':'keypress', 'charCode':chrCode});
+}
+$(document).ready(function () {
+	$("input").keypress(function (e) { inputHandler(e); } );
+	$("span").click(function (e) { spanHandler(e); } );
+});
