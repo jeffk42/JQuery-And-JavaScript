382b812ae769b04d96fb57a285910ff222f22902
Jeff Kraus <jeffk42@gmail.com>
2017-09-07 07:50:47 -0400

    hour 1201

---
+++ hour12/hour1201.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$("#handle").click(toggleImage);
+});
+
+function toggleImage() {
+	if ($("#handle").html() == "+") {
+		$("#photo").show(1000, function () { $("#footer").show() });
+		$("#handle").html("-");
+	}
+	else {
+		$("#footer").hide();
+		$("#photo").hide(1000);
+		$("#handle").html("+");
+	}
+}
